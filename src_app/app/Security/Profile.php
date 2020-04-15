f75a52be12bfe497fb6f7e059af5b78b5337e66e
Santiago Palacios <s.palacios@itecriocuarto.org.ar>
2020-04-15 01:46:51 -0300

    practico

---
+++ src_app/app/Security/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security;
+
+class Profile
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+}
